d421330a874477aa23f04bb9a1a1af8a47c29605
bdey05 <73259986+bdey05@users.noreply.github.com>
2023-08-15 10:55:14 -0400

    Create pH_calculator.py

---
+++ pH_calculator.py
@@ -0,0 +1,34 @@
+import json
+import numpy as np
+import math
+
+def alpha():
+    try:
+        with open('all_TPEN_DERIVATIVE_records.json', 'r', encoding = 'utf-8') as f:
+    #the load function will directly load and parse the file, creating a dictionary 
+            json_data = json.load(f)
+
+            filtered_data = []
+
+            for aqueous in json_data["aqueous phase"]:
+                aqueous_data = {
+                "solutes" == aqueous["solutes"],
+                "pH" == aqueous["pH"],
+                "volume" == aqueous["volume"],}
+                            
+            filtered_data.append(aqueous_data)
+                        
+            if isinstance("pH", (int, float)):
+                pH = np.array("pH", dtype=float)
+                            
+                h3o = -math.log10(pH)
+                return h3o
+            else:
+                return None
+            
+    except ValueError:
+        print('Decoding JSON has failed')
+
+    print(h3o)
+
+    alpha()
