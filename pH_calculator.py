14033a78429565c0a12f5431def9265f657752ea
bdey05 <73259986+bdey05@users.noreply.github.com>
2023-08-18 00:34:20 +0000

    Added file validators

--- pH_calculator.py
+++ pH_calculator.py
@@ -1,31 +1,29 @@
 import json
 import numpy as np
-import math
+import math 
+
+def alpha(file_name):
+
+    with open(file_name, "r",  encoding = 'utf-8') as file:
+    
+        json_data = json.load(file)
+    
+    filtered_data = []
+    
+    if (isinstance(json_data, list)):
+        for record in json_data:
+            #print(record['aqueous phase']['solutes'])
+            filtered_data.append(record["aqueous phase"])
+
+    if (isinstance(json_data, dict)):
+        #print(json_data["aqueous phase"])
+        filtered_data.append(json_data["aqueous phase"])
+
+    for record in filtered_data:
+        if (isinstance(record['pH'], int)):
+            print("pH is present and valid")
+        else:
+            print("pH is not int. Here are the solutes:", record['solutes'])
 
 
-def alpha():
-    try:
-        with open('all_TPEN_DERIVATIVE_records.json', 'r', encoding = 'utf-8') as f:
-    #the load function will directly load and parse the file, creating a dictionary 
-            json_data = json.load(f)
-
-            filtered_data = []
-
-            for record in json_data:
-                filtered_data.append(record["aqueous phase"])
-                              
-            if isinstance("pH", (int, float)):
-                pH = np.array("pH", dtype=float)
-                            
-                h3o = -math.log10(pH)
-                return h3o
-            else:
-                return None
-            
-    except ValueError:
-        print('Decoding JSON has failed')
-
-    print(h3o)
-
-
-alpha()
+alpha("all_TPEN_DERIVATIVE_records.json")
